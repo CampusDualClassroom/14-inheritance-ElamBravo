b05b10fe9d15b6f8415c98752302ca06e78f2f44
Elam Bravo <bravoelam@gmail.com>
2025-11-05 10:40:18 +0100

    Solved Exercise 14

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -6,5 +6,15 @@ public class Exercise {
         Teacher t = new Teacher("Maria", "Montessori", "Educación");
         PoliceOfficer po = new PoliceOfficer("Jake", "Peralta", "B-99");
         Doctor d = new Doctor("Gregory", "House", "Nefrología e Infectología");
+
+        p.getDetails();
+            System.out.println("_________");
+        t.getDetails();
+            System.out.println("_________");
+        po.getDetails();
+            System.out.println("_________");
+        d.getDetails();
+            System.out.println("__________");
     }
+
 }
